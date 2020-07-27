29fce180717c31248c4b2e66579ce1e8ddfe9634
crisebas <cgarciap@uni.pe>
2020-07-27 05:33:43 -0500

    some bugs fixed

--- producer.py
+++ producer.py
@@ -2,11 +2,11 @@ def establish_connection(SOCKET, HOST, PORT):
     SOCKET.connect((HOST, PORT))
 
 def declare_queue(SOCKET, action, queue_name):
-    message_to_server = "DECLARE_QUEUE/" + action + "/" + queue_name
+    message_to_server = "DECLARE_QUEUE/" + action + "/" + queue_name + "\n"
     SOCKET.sendall(message_to_server.encode())
 
 def send_message(SOCKET, message, queue_name):
-    message_to_server = "MESSAGE/" + message + "/" + queue_name
+    message_to_server = "MESSAGE/" + message + "/" + queue_name + "\n"
     SOCKET.sendall(message_to_server.encode())
 
 def disconnect(SOCKET):
